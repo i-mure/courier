a85a778204d7ac22d1ee95daedb49cc19f3e4b94
Brian Keyonzo <briankeyonzo@yahoo.com>
2017-03-16 00:28:33 +0300

    Added receipt template

--- courier/templates.py
+++ courier/templates.py
@@ -52,6 +52,7 @@ class ButtonTemplate:
 			}
 		}
 
+
 class ListTemplate:
 	"""
 	List template
@@ -76,4 +77,44 @@ class ListTemplate:
 		}
 		if self.button is not None: json["buttons"] = [self.button]
 		if self.top_element_style is not None: json["top_element_style"] = self.top_element_style
+		return json
+
+
+class ReceiptTemplate:
+	def __init__(self, recipient_name, order_number, currency, payment_method, summary,
+				 merchant_name = None, timestamp = None, order_url = None, elements = None,
+				 address = None, adjustments = None):
+		self.recipient_name = recipient_name
+		self.merchant_name = merchant_name
+		self.order_number = order_number
+		self.currency = currency
+		self.payment_method = payment_method
+		self.timestamp = timestamp
+		self.order_url = order_url
+		self.elements = elements
+		self.address = address
+		self.summary = summary
+		self.adjustments = adjustments
+
+
+	def to_json(self):
+		json = {
+			"attachment": {
+				"type": "template",
+				"payload":{
+					"template_type": "receipt",
+					"recipient_name": self.recipient_name,
+					"order_number": self.order_number,
+					"currency": self.currency,
+					"payment_method": self.payment_method,
+					"summary": self.summary
+				}
+			}
+		}
+		if self.merchant_name is not None: json["merchant_name"] = self.merchant_name
+		if self.timestamp is not None: json["timestamp"] = self.timestamp
+		if self.order_url is not None: json["order_url"] = self.order_url
+		if self.elements is not None: json["elements"] = self.elements
+		if self.address is not None: json["address"] = self.address
+		if self.adjustments is not None: json["adjustments"] = self.adjustments
 		return json
